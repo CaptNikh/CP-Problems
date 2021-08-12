368cbe6d0c64b49b81119f8fc694f2e65c0aa5e9
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2021-08-12 20:14:20 +0530

    Update CHFGCD.cpp

--- CODECHEF/COOKOFF/July div2/CHFGCD.cpp
+++ CODECHEF/COOKOFF/July div2/CHFGCD.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int chfgcd() {
+    int x, y;
+    cin >> x >> y;
 
+    if (__gcd(x, y) > 1)
+        return 0;
+    else if (__gcd(x + 1, y) > 1 || __gcd(x, y + 1) > 1)
+        return 1;
+    else
+        return 2;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cout << chfgcd() << "\n";
+    }
+    return 0;
+}
