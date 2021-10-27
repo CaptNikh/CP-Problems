36cf98261c672c775c6149344915d3a43fd0aee2
Nikhil Kumar Mishra <Nikhil.mishra.sf@gmail.com>
2021-10-27 23:52:48 +0530

    Create N1VALUES.cpp

---
+++ CODECHEF/LUNCHTIME/octDIv2/N1VALUES.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+
+#define INF INT_MAX
+#define MOD 1000000007
+#define all(x) x.begin(), x.end()
+#define MAXN 1000000
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define int long long
+
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int x;
+		cin >> x;
+		int res = pow(2, x);
+
+		int i = 1;
+		for (i = 1 ; i < x; i++)
+		{
+			res = res - i;
+			cout << i << " ";
+		}
+		if (i == 1)
+		{
+			res = res - i;
+			cout << i << " ";
+		}
+
+		else {
+
+			res = res - (i - 1);
+			cout << i - 1 << " ";
+		}
+
+		cout << res << "\n";
+
+	}
+
+
+	return 0;
+}
